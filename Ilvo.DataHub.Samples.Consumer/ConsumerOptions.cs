3477c58daec3465f798e9f77f5ad811be2d1dc27
Tycho Vercauteren <tycho.vercauteren@cegeka.com>
2019-08-28 15:23:49 +0200

    Created base consumer example

---
+++ Ilvo.DataHub.Samples.Consumer/ConsumerOptions.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Ilvo.DataHub.Samples.Consumer
+{
+    [Verb("consume", HelpText = "consumes given api call")]
+    public class ConsumerOptions
+    {
+        [Option('u', "url", Required = true, HelpText = "")]
+        public Uri Url { get; set; }
+
+        [Option('v', "verb", Required = true, HelpText = "HTTP verb")]
+        public HttpMethod Verb { get; set; }
+
+        [Option('c', "certificate path", Required = true, HelpText = "path to the client certificate")]
+        public string CertificatePath { get; set; }
+
+        [Option('s', "subscriptionkey", Required = true, HelpText = "Subscriptionkey")]
+        public string SubscriptionKey { get; set; }
+
+        [Option('p', "purpose", Required = true, HelpText = "purpose")]
+        public string Purpose { get; set; }
+    }
+}
---
+++ Ilvo.DataHub.Samples.Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Ilvo.DataHub.Samples.Consumer
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            return await Parser.Default.ParseArguments<ConsumerOptions>(args)
+                    .MapResult(
+                        (opts) => CallAPI(opts),
+                        errs => Task.FromResult(-1));
+        }
+
+        private static async Task<int> CallAPI(ConsumerOptions opts)
+        {
+            var certificate = new X509Certificate();
+            certificate.Import(opts.CertificatePath);
+
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ClientCertificates.Add(certificate);
+            clientHandler.ClientCertificateOptions = ClientCertificateOption.Manual;
+
+            using (var client = new HttpClient(clientHandler))
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", opts.SubscriptionKey);
+                if (!string.IsNullOrEmpty(opts.Purpose))
+                    client.DefaultRequestHeaders.Add("purpose", opts.Purpose);
+
+                var request = new HttpRequestMessage(opts.Verb, opts.Url);
+                var response = await client.SendAsync(request);
+
+                Console.Write(JsonConvert.SerializeObject(response));
+                return response.IsSuccessStatusCode ? 0 : 1;
+            }
+        }
+    }
+}
