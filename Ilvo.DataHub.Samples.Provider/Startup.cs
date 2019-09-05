1bf9a3a75ca9b78bfb76750a65e74f06c7a16785
Tim Pauwels <tim.pauwels@cegeka.com>
2019-09-05 14:52:21 +0200

    Added OpenApi spec and Swagger UI

--- Ilvo.DataHub.Samples.Provider/Startup.cs
+++ Ilvo.DataHub.Samples.Provider/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Ilvo.DataHub.Samples.Provider
 {
@@ -26,11 +27,45 @@ namespace Ilvo.DataHub.Samples.Provider
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "ILVO DataHub Sample Provider API",
+                    Description = "Sample of a Provider API, consumed by the ILVO DataHub.",
+                    Version = "v1",
+                    //TermsOfService = new Uri("https://example.com/terms"),
+                    //Contact = new Contact()
+                    //{
+                    //    Name = "Shayne Boyer",
+                    //    Email = string.Empty,
+                    //    Url = new Uri("https://twitter.com/spboyer"),
+                    //},
+                    //License = new License
+                    //{
+                    //    Name = "Use under LICX",
+                    //    Url = new Uri("https://example.com/license"),
+                    //}
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ILVO DataHub Sample Provider API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
