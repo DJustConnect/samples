4ce0530eac1161d61190465da81ffb2cc0d0aea2
Tycho Vercauteren <tycho.vercauteren@cegeka.com>
2019-10-28 09:37:16 +0100

    Added farm id call example

---
+++ Ilvo.DataHub.Samples.Provider/Controllers/NumberController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Ilvo.DataHub.Samples.Provider.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ilvo.DataHub.Samples.Provider.Controllers
+{
+    [RequireClientCertificate]
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(typeof(void), (int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType(typeof(void), (int)HttpStatusCode.Forbidden)]
+    public class NumberController : ControllerBase
+    {
+        /// <summary>
+        /// Returns all the values.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<int>), (int)HttpStatusCode.OK)]
+        public ActionResult<IEnumerable<int>> Get()
+        {
+            return new int[] { 1, 12, 35 };
+        }
+
+        /// <summary>
+        /// Returns pi.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("pi")]
+        [ProducesResponseType(typeof(IEnumerable<int>), (int)HttpStatusCode.OK)]
+        public ActionResult<double> GetPi()
+        {
+            return Math.PI;
+        }
+    }
+}
