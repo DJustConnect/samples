239fbe087a4f4b8004622720e2a2577f5cefa1e4
Tycho Vercauteren <tycho.vercauteren@cegeka.com>
2020-04-02 14:26:30 +0200

    Added push notifications controller

---
+++ Ilvo.DataHub.Samples.Provider/Controllers/PushNotificationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ilvo.DataHub.Samples.Provider.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+
+namespace Ilvo.DataHub.Samples.Provider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PushNotificationController : ControllerBase
+    {
+        [HttpPost("event")]
+        public async Task<IActionResult> CreateAndPublishEvent([FromBody] Event e)
+        {
+            var topicCredentials = new TopicCredentials(e.Key);
+            using (var client = new EventGridClient(topicCredentials))
+                await client.PublishEventsAsync(new Uri(e.Topic).Host, CreateEvent(e));
+
+            return Ok();
+        }
+
+        private static IList<EventGridEvent> CreateEvent(Event e)
+        {
+            return new List<EventGridEvent>
+            {
+                new EventGridEvent
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Data = e.Payload,
+                    EventTime = DateTime.Now,
+                    EventType = e.EventType,//The resource Id
+                    Subject = e.Subject, //The farm number
+                    DataVersion = "1.0"
+                }
+            };
+        }
+    }
+}
---
+++ Ilvo.DataHub.Samples.Provider/Models/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace Ilvo.DataHub.Samples.Provider.Models
+{
+    public class Event
+    {
+        public string Topic { get; set; }
+        public string EventType { get; set; }
+        public string Subject { get; set; }
+        public string Key { get; set; }
+        public object Payload { get; set; }
+    }
+}
